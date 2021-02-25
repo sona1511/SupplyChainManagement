2d284e2e3daf3b3335a74063bb13544cb046df4a
sona1511 <sacyadav2005@gmail.com>
2021-02-25 17:50:27 +0530

    Third Commit

---
+++ springboot-backend/src/test/java/com/sona/springboot/test/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sona.springboot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
